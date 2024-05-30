e23c00fd96b4b65c21d14628a4fb7280a2c2be93
Louis-Marie Baer <lm@baermail.fr>
2024-05-30 16:17:32 +0200

    fix: script install

--- docs/quick-install.sh
+++ docs/quick-install.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 if grep -q Debian /etc/os-release
 then
+  sudo apt install wget -y
   # crate binary and config directories
   sudo mkdir -p /opt/api-formatter /etc/api-formatter
   # set right permission to directories
   sudo chown -R $USER /opt/api-formatter /etc/api-formatter
   # download binary
-  wget https://github.com/Cyrix126/api-formatter/releases/download/v0.1.0/api-formatter-linux-amd64 /opt/api-formatter/
+  wget -q -P /opt/api-formatter/ https://github.com/Cyrix126/api-formatter/releases/download/v0.1.0/api-formatter-linux-amd64
   # set execution permission for binary
   sudo chmod +x /opt/api-formatter/api-formatter-linux-amd64
   # download configuration
-  wget https://raw.githubusercontent.com/Cyrix126/api-formatter/docs/config.toml /etc/api-formatter/config.toml
+  wget -q -P /etc/api-formatter/config.toml https://raw.githubusercontent.com/Cyrix126/api-formatter/main/docs/config.toml
   # download systemd service
-  wget https://raw.githubusercontent.com/Cyrix126/api-formatter/docs/api-formatter.service /etc/systemd/system/api-formatter.service
-  # reload systemctl and enable service at boot
+ sudo wget -q -P /etc/systemd/system https://raw.githubusercontent.com/Cyrix126/api-formatter/main/docs/api-formatter.service   # reload systemctl and enable service at boot
   sudo systemctl-daemon reload
   sudo systemctl enable api-formatter
   sudo systemctl stop api-formatter
